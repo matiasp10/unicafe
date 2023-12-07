11ebbf2f8d79edbf7072f1202b734b6f574c3a56
Matias Petenatti <matiasodin96@gmail.com>
2023-12-06 23:59:04 -0300

    1.9

--- components/Statistics.jsx
+++ components/Statistics.jsx
@@ -1,20 +1,24 @@
 // eslint-disable-next-line react/prop-types
 function Statistics({ bad, good, neutral }) {
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <span>Good: {good}</span> <br />
-      <span>Neutral: {neutral}</span>
-      <br />
-      <span>Bad: {bad}</span>
-      <br />
-      <span>Total: {bad + good + neutral}</span>
-      <br />
-      <span>Promedio: {(good - bad) / (bad + good + neutral)}</span>
-      <br />
-      <span>Positive: {(good * 100) / (bad + good + neutral)} %</span>
-    </div>
-  );
+  if (bad + good + neutral === 0) {
+    return <div>No feedback given</div>;
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <span>Good: {good}</span> <br />
+        <span>Neutral: {neutral}</span>
+        <br />
+        <span>Bad: {bad}</span>
+        <br />
+        <span>Total: {bad + good + neutral}</span>
+        <br />
+        <span>Promedio: {(good - bad) / (bad + good + neutral)}</span>
+        <br />
+        <span>Positive: {(good * 100) / (bad + good + neutral)} %</span>
+      </div>
+    );
+  }
 }
 
 export default Statistics;
